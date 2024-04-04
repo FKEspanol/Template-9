889413a1f22acce77fac1ecd9fb04d3c7ac4db06
karlo <karlsgit1126@gmail.com>
2024-04-04 17:11:37 +0800

    added tools and libraries, added responsive navbar

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = "FutureFusion";
+export const SITE_DESCRIPTION =
+    " FutureFusion Labs is a visionary startup shaping the future through fusion of technology and creativity. ";
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            screens: {
+                sm: "576px",
+                md: "768px",
+                lg: "992px",
+                xl: "1200px",
+                "2xl": "1400px",
+            },
+            colors: {
+                primary: "#FEB900",
+                secondary: "#BE8B55",
+                dark: "#191919",
+                default: "#9B9A9D",
+                info: "#535153",
+                success: "#78AA4C",
+                warning: "#F59A14",
+                danger: "#F44336",
+            },
+        },
+    },
+    plugins: [],
+};
